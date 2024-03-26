6828cc629281f1be1c415a835bce6adf3e5d93dd
Klim Hurdakovgit config --global user.email mr.khurdakov@mail.ru <mr.khurdakov@mail.ru>
2024-03-26 22:31:50 +0300

    3 lab

---
+++ lab3app/app/src/main/java/com/example/lab3app/MainActivity.kt
@@ -0,0 +1,90 @@
+package com.example.lab3app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.fragment.app.Fragment
+import com.example.lab3app.fragments.FacultyListFragment
+import com.example.lab3app.fragments.UniversityListFragment
+import com.example.lab3app.fragments.UpdateActivity
+import com.example.lab3app.repository.UniversityRepository
+
+class MainActivity : AppCompatActivity(), UpdateActivity {
+
+
+    companion object{
+        const val universityID = 0
+        const val facultyID = 1
+    }
+
+    interface Edit{
+        fun append()
+        fun update()
+        fun delete()
+    }
+
+    private var _miNewUniversity: MenuItem? = null
+    private var _miUpdateUniversity: MenuItem? = null
+    private var _miDeleteUniversity: MenuItem? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction().replace(R.id.fcwMain, UniversityListFragment.getInstance()).commit()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        _miNewUniversity = menu?.findItem(R.id.miNewUniversity)
+        _miUpdateUniversity = menu?.findItem(R.id.miUpdateUniversity)
+        _miDeleteUniversity = menu?.findItem(R.id.miDeleteUniversity)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId){
+            R.id.miNewUniversity ->{
+                val fedit: Edit = UniversityListFragment.getInstance()
+                fedit.append()
+                true
+            }
+            R.id.miUpdateUniversity ->{
+                val fedit: Edit = UniversityListFragment.getInstance()
+                fedit.update()
+                true
+            }
+            R.id.miDeleteUniversity ->{
+                val fedit: Edit = UniversityListFragment.getInstance()
+                fedit.delete()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onStop() {
+        UniversityRepository.getInstance().saveData()
+        super.onStop()
+    }
+
+    override fun setTitle(_title: String) {
+        title = _title
+    }
+
+    private var currentFragmentID = -1
+    override fun setFragment(fragmentID: Int) {
+        currentFragmentID = fragmentID
+        when(fragmentID){
+            universityID->{setFragment(UniversityListFragment.getInstance())}
+            facultyID->{setFragment(FacultyListFragment.getInstance())}
+        }
+    }
+
+    private fun setFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fcwMain,fragment)
+            .commit()
+    }
+}
---
+++ lab3app/app/src/main/java/com/example/lab3app/fragments/UpdateActivity.kt
@@ -0,0 +1,6 @@
+package com.example.lab3app.fragments
+
+interface UpdateActivity {
+    fun setTitle(_title: String)
+    fun setFragment(fragmentID : Int)
+}
